15cfc80d3594e9ab5c80db3c90b73e8543095907
hannako <jessjones1@gmail.com>
2016-07-04 22:17:29 +0100

    fizzbuzz 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,23 +1,22 @@
 def fizzbuzz(n)
-return 'fizzbuzz' if is_divisible_by_fifteen?(n)
-return 'buzz' if is_divisible_by_five?(n)
-return 'fizz' if is_divisible_by_three?(n)
+  return 'fizzbuzz' if is_divisible_by_fifteen?(n)
+  return 'fizz' if is_divisible_by_three?(n)
+  return 'buzz' if is_divisible_by_five?(n)
   n
 end
 
+  def is_divisible_by_three?(n)
+    is_divisible_by?(n,3)
+  end
 
-def is_divisible_by_three?(n)
-  is_divisible_by?(n, 3)
-end
+  def is_divisible_by_five?(n)
+    is_divisible_by?(n,5)
+  end
 
-def is_divisible_by_five?(n)
-  is_divisible_by?(n, 5)
-end
+  def is_divisible_by_fifteen?(n)
+    is_divisible_by?(n,15)
+  end
 
-def is_divisible_by_fifteen?(n)
-  is_divisible_by?(n, 15)
-end
-
-def is_divisible_by?(n,divisor)
-  n % divisor == 0
-end
+  def is_divisible_by?(n,divisor)
+    n % divisor == 0
+  end
