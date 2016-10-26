16b3a78122c600b4ade2fb08f6a48abf91c6fe1c
hannako <jessjones1@gmail.com>
2016-10-26 22:18:32 +0100

    9

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,50 +1,45 @@
 require 'fizzbuzz'
+
 describe 'fizzbuzz' do
-
-  context 'knows when a number is divisible by' do
+  context 'it knows when a number is divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(3)).to be true
+      expect(is_divisible_by_three?(3)).to eq true
     end
     it '5' do
-      expect(is_divisible_by_five?(5)).to be true
+      expect(is_divisible_by_five?(5)).to eq true
     end
     it '15' do
-      expect(is_divisible_by_fifteen?(15)).to be true
+      expect(is_divisible_by_fifteen?(15)).to eq true
     end
   end
 
-  context 'knows when a number is not divisible by' do
+  context 'it knows when a number is not divisible by' do
     it '3' do
-      expect(is_divisible_by_three?(4)).to be false
+      expect(is_divisible_by_three?(4)).to eq false
     end
+
     it '5' do
-      expect(is_divisible_by_five?(4)).to be false
+      expect(is_divisible_by_five?(4)).to eq false
     end
+
     it '15' do
-      expect(is_divisible_by_fifteen?(4)).to be false
+      expect(is_divisible_by_fifteen?(4)).to eq false
     end
-
-    context 'when playing the game' do
-      it 'returns the number' do
-        expect(fizzbuzz(1)).to eq 1
-      end
-
-      it 'returns fizz' do
-        expect(fizzbuzz(3)).to eq 'fizz'
-      end
-
-      it 'returns buzz' do
-        expect(fizzbuzz(5)).to eq 'buzz'
-      end
-
-      it 'returns fizzbuzz' do
-        expect(fizzbuzz(15)).to eq 'fizzbuzz'
-      end
-
-
-    end
-
-
   end
 
+  context 'when playing a game of fizzbuzz it returns' do
+    it 'fizz' do
+      expect(fizzbuzz(3)).to eq "fizz"
+    end
+    it 'buzz' do
+      expect(fizzbuzz(5)).to eq "buzz"
+    end
+    it 'fizzbuzz' do
+      expect(fizzbuzz(15)).to eq "fizzbuzz"
+    end
+
+    it 'number' do
+      expect(fizzbuzz(1)).to eq 1
+    end
+  end
 end
